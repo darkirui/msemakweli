016e35be93abd575eaf1162d39824466d0d5a9f3
Kiprotich Darius <dariuskirui@gmail.com>
2022-02-10 08:28:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php include 'header.php'; ?>
+<div class="homebanner">
+	<div class="contentwrap">
+		<div class="homebannercontentwrap">
+			<div class="tablediv">
+				<div class="tablerowdiv">
+					<div class="tablecelldiv">
+						<div class="hometag">Get Facts. Get Tested. Get Involved...</div>
+						<div class="searchwrap">
+							<div class="tablediv">
+								<div class="tablerowdiv">
+									<div class="tablecelldiv homesearchwrap">
+										<input type="text" class="homesearch" placeholder="Any Indicator">
+									</div>
+									<div class="tablecelldiv homesearchbtnwrap">
+										<button class="homesearchbtn">Find</button>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="summarysection">
+	<div class="contentwrap">
+		<div class="summarysectionwarp">
+			<div class="tablediv">
+				<div class="tablerowdiv">
+					<div class="tablecelldiv summarycontentcell">
+						<div class="summaryvalue">57</div>
+						<div class="summaryvaluedescription">Health Facilities partnering with KARP</div>
+					</div>
+					<div class="tablecelldiv summarycontentseparator">
+					</div>
+					<div class="tablecelldiv summarycontentcell">
+						<div class="summaryvalue">90,000</div>
+						<div class="summaryvaluedescription">And more people on Antitroviral Treatment</div>
+					</div>
+					<div class="tablecelldiv summarycontentseparator">
+					</div>
+					<div class="tablecelldiv summarycontentcell">
+						<div class="summaryvalue">100,000</div>
+						<div class="summaryvaluedescription">And more received HIV Counselling and Testing</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
